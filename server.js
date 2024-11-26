9427f33a7829df0279f72d7b1345437e688297f1
Nguyen Ly <lyphtec@gmail.com>
2024-11-27 00:45:25 +1100

    - Docdash options - Server middleware ordering

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ import morgan from 'morgan';
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set("host", "http://localhost");
-app.use(express.static('out'));
+app.set("host", "localhost");
 app.use(morgan('dev'));
+app.use(express.static('out'));
 
 app.listen(app.get('port'), () => {
-    var h = `${(app.get('host') || hostname() || 'unknown')}:${app.get('port')}`;
+    var h = `http://${(app.get('host') || hostname() || 'unknown')}:${app.get('port')}`;
     console.log(`Express server listening at ${h}`);
 
-    open("http://localhost:3000");
+    open(h);
 });
